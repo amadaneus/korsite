897152d10db987d400ab77a1abcd9879e1ecd788
amadaneus <verrtah@gmail.com>
2018-03-19 09:46:41 -0500

    Updates Routes, creating namespace for Admin Panel feature and routing for Blog feature.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+
+  get 'comments/new'
+
+  get 'categories', to: 'categories#index'
+
+  get 'blog', to: 'posts#index'
+
+  get 'posts/show'
+
   get 'about', to: 'pages#about'
 
   get 'contact', to: 'messages#new', as: 'new_message'
@@ -7,6 +17,20 @@ Rails.application.routes.draw do
 
   get 'registration', to: 'registrations#new', as: 'new_registration'
   post 'registration', to: 'registrations#create', as: 'create_registration'
+
+  resources :posts, :categories, :comments
+
+  namespace :admin do
+    get 'sessions/new'
+  end
+
+  namespace :admin do
+    get 'comments/destroy'
+  end
+
+  namespace :admin do
+    resources :posts, :categories, :comments, :users
+  end
 
   root to: 'pages#home'
 

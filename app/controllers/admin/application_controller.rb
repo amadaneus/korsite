e130d93bfc5ad6c2b99cab8dd186d158803a5182
amadaneus <verrtah@gmail.com>
2018-03-20 15:05:21 -0500

    Adds DeviseWhitelist concern to Application and Admin Application controllers.

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
+  protect_from_forgery
+  include 'DeviseWhitelist'
   layout 'admin/application'
 end

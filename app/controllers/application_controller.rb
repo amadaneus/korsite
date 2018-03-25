d20ed6a9300de4cd55cafa6166de297dd0f9d0b1
amadaneus <verrtah@gmail.com>
2018-03-25 18:30:28 -0500

    Implements basic authorization and authentication functionlity into Admin Panel and Blog Features.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
-  include CurrentUserConcern
+  # include CurrentUserConcern
   layout 'blog'
   layout 'application'
 

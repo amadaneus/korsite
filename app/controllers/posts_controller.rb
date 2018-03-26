2565d5b23c8341caeb6dadd59cb37172ab9189b9
amadaneus <verrtah@gmail.com>
2018-03-26 13:53:29 -0500

    Implements filter in Posts index controller to only show published posts in Blog feature using enum.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,15 +3,15 @@ class PostsController < ApplicationController
 
   def index
     if params[:search]
-      @posts = Post.search(params[:search]).page(params[:page]).per(5).order('created_at DESC')
+      @posts = Post.published.search(params[:search]).page(params[:page]).per(5).order('created_at DESC')
     else
-      @posts = Post.page(params[:page]).per(5).order('created_at DESC')
+      @posts = Post.published.page(params[:page]).per(5).order('created_at DESC')
     end
     @categories = Category.all
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post = Post.published.find(params[:id])
     @categories = Category.all
   end
 

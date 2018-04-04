aee0c603160fcc25079a88ffb917dbeff5fb308f
amadaneus <verrtah@gmail.com>
2018-04-04 09:47:20 -0500

    Updates styling in css and js, makes navbar link color responsive to scrollbar positioning and resizes footer text.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,10 +23,12 @@
 $(document).ready(function() {
     $(window).scroll(function() {
         if($(document).scrollTop() > 10) {
-            $('#nav').addClass('shrink');
+            $('#nav').removeClass('navbar-dark');
+            $('#nav').addClass('shrink navbar-light');
         }
         else {
-            $('#nav').removeClass('shrink');
+            $('#nav').removeClass('shrink navbar-light');
+            $('#nav').addClass('navbar-dark');
         }
     });
 
